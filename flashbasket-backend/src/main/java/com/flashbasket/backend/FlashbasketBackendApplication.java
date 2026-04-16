b582829ebcf40b0a3035c1df41e460d167b5f88d
minakshi <minakshiwaghmare1411@gmail.com>
2026-04-16 12:03:36 +0530

    Initial commit - FlashBasket backend setup

---
+++ flashbasket-backend/src/main/java/com/flashbasket/backend/FlashbasketBackendApplication.java
@@ -0,0 +1,13 @@
+package com.flashbasket.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashbasketBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashbasketBackendApplication.class, args);
+	}
+
+}
---
+++ flashbasket-backend/src/test/java/com/flashbasket/backend/FlashbasketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashbasket.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashbasketBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
